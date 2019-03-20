c77d7d30e1d6288dffc2020d47cafb9f65d06eab
FabiolaSaga <saga.fabiola@gmail.com>
2019-03-20 14:27:02 -0500

    Added hitTestResult to the plane.

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -44,15 +44,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
 //        sceneView.showsStatistics = true
         
-//        // Create a new scene
-//        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
-//
-//        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true){
-//
-//        diceNode.position = SCNVector3(0, 0, -0.1)
-//
-//        sceneView.scene.rootNode.addChildNode(diceNode)
-//        }
 //
 //        // Set the scene to the view
 //        sceneView.scene = scene
@@ -82,10 +73,20 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             
             let results = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
             
-            if !results.isEmpty {
-                print("touched the plane")
-            } else {
-                print("touched somewhere else")
+            if let hitResult = results.first {
+                //        // Create a new scene
+                let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+        
+                if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true){
+        
+                diceNode.position = SCNVector3(
+                    hitResult.worldTransform.columns.3.x,
+                    hitResult.worldTransform.columns.3.y + diceNode.boundingSphere.radius,
+                    hitResult.worldTransform.columns.3.z
+                    )
+        
+                sceneView.scene.rootNode.addChildNode(diceNode)
+                }
             }
         }
     }
